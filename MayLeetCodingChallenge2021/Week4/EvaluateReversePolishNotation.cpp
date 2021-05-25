c977599a3df6161a2a9777f57973ab052c63d89c
carlos <geral@meuppt.pt>
2021-05-25 17:30:29 +0100

    New entry

---
+++ MayLeetCodingChallenge2021/Week4/EvaluateReversePolishNotation.cpp
@@ -0,0 +1,43 @@
+class Solution {
+    
+public:
+    int evalRPN(vector<string>& tokens) {
+        
+        stack<int> s;
+
+        for(auto& t : tokens) 
+            
+            if(t == "+" or 
+               t == "-" or 
+               t == "*" or 
+               t == "/") {
+                
+                int op1 = s.top(); s.pop();
+                int op2 = s.top(); s.pop();
+                
+                if(t == "+") {
+                    op1 = op2 + op1;
+                }
+
+                if(t == "-") {
+                    op1 = op2 - op1;
+                }
+
+                if(t == "/") {
+                    op1 = op2 / op1;
+                }
+
+                if(t == "*") {
+                    op1 = op2 * op1;
+                }
+                
+                s.push(op1);
+                
+            } else {
+
+                s.push(stoi(t));
+            }
+
+        return s.top();       
+    }
+};
