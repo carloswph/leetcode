b55a3ca88f3bb6d7ea13b26c045bf3ad5b1a7872
carlos <geral@meuppt.pt>
2021-05-25 01:33:33 +0100

    Init

---
+++ MayLeetCodingChallenge2021/Week4/ToLowerCase.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include <locale>
+
+class Solution {
+public:
+    string toLowerCase(string s) {
+    	
+        std::transform(s.begin(), s.end(), s.begin(), [](unsigned char c){ 
+        	return std::tolower(c); 
+        });
+
+        return s;
+    }
+};
